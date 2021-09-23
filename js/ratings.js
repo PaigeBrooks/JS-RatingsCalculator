db098f90356caa0478d5dface3b7ca49fafb52f5
PaigeBrooks <pbrooks@sterlingsolutions.co.uk>
2021-09-23 16:13:47 +0100

    Everything else

--- js/ratings.js
+++ js/ratings.js
@@ -10,4 +10,9 @@ function collect_ratings(){
     if(ratings.count != 0){
         ratings.average = ratings.sum/ratings.count;
     }
-}
+    return ratings;
+};
+document.addEventListener('change', () => {
+    const ratings = collect_ratings();
+    document.querySelector('#average').value = ratings.average.toFixed(2);
+  });
